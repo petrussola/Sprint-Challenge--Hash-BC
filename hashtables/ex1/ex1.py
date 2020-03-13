259ce51b59df44a3fa479c0426a42417579507a4
Pere <pere.solaclaver@gmail.com>
2020-03-13 10:16:33 +0000

    ex1 hashtable except for test 2

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -12,6 +12,19 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
+    for i in range(length):
+        if hash_table_retrieve(ht, weights[i]) == None:
+            hash_table_insert(ht, weights[i], i)
+
+    for i in range(len(weights)):
+        difference = limit - weights[i]
+        if hash_table_retrieve(ht, difference) != None:
+            index = hash_table_retrieve(ht, difference)
+
+            if i >= index:
+                return (i, index)
+            else:
+                return (index, i)
 
     return None
 
